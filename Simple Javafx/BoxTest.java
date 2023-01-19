a2a1bc328afcbbb54f2255b5ef0d02500f9cb677
Bruno Stevaux <brnstvx@gmail.com>
2023-01-19 17:22:56 -0400

    Renaming and Sorting

---
+++ Simple Javafx/BoxTest.java
@@ -0,0 +1,48 @@
+//********************************************************************
+//  BoxTest.java       Author: Lewis/Loftus
+//
+//  Solution to Programming Project 4.6
+//********************************************************************
+
+public class BoxTest
+{
+    //-----------------------------------------------------------------
+    //  Creates and exercises some Box objects.
+    //-----------------------------------------------------------------
+    public static void main(String[] args)
+    {
+        Box b1 = new Box(4, 10, 7);
+        Box b2 = new Box(3, 3, 4);
+        Box b3 = new Box(12, 8, 8);
+
+        System.out.println(b1);
+        System.out.println();
+        System.out.println(b2);
+        System.out.println();
+        System.out.println(b3);
+
+        b1.setHeight(10);
+        b2.setWidth(14);
+        b3.setDepth(11);
+        b2.setFull(true);
+
+        System.out.println();
+        System.out.println(b1);
+        if (b1.isFull())
+            System.out.println("Please use another box.");
+        else
+            System.out.println("You can use it.");
+          
+        System.out.println();
+        System.out.println(b2);
+        if (b2.isFull())
+            System.out.println("Please use another box.");
+        else
+            System.out.println("You can use it.");
+          
+        System.out.println();
+        System.out.println(b3);
+        
+        
+    }
+}
